17e07060160fa8287af48c00aabe8871fc63f299
otokarsefer <otokar.sefer@voco.ee>
2024-09-05 13:09:11 +0300

    zxcz

--- yl2.2.js
+++ yl2.2.js
@@ -1,14 +1,22 @@
-const readline = require('node:readline')
+const readline = require('node:readline');
+
 const info = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-info.question("Sisesta nimi: ", nimi => { 
-    info.close()})
+info.question("Sisesta nimi: ", (nimi) => {
+  info.close();
 
-if(info.nimi.slice(-1) != "e"){console.log("Pole ilmselt leedulanna perekonna nimi....")} 
-else{
-if(info.nimi.slice(-2) != "ne"){console.log("Abielus")}
-if(info.nimi.slice(-2) != "te"){console.log("Vallaline")}
-else{console.log("Määramatta")}
-} 
+  if (nimi.slice(-1) !== "e") {
+    console.log("Pole ilmselt leedulanna perekonna nimi....");
+  } else {
+    if (nimi.slice(-2) === "ne") {
+      console.log("Abielus");
+    } else if (nimi.slice(-2) === "te") {
+      console.log("Vallaline");
+    } else {
+      console.log("Määramatta");
+    }
+  }
+});
